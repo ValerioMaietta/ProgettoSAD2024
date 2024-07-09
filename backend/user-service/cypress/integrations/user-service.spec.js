419f6c5918df32cb2adb2eb1d9a9a7ac979f8be0
daniloromano99 <72809334+daniloromano99@users.noreply.github.com>
2024-07-09 16:15:59 +0200

    commit cypress user-service risolto

--- backend/user-service/cypress/integrations/user-service.spec.js
+++ backend/user-service/cypress/integrations/user-service.spec.js
@@ -1,52 +1,42 @@
-// cypress/integration/user-service.spec.js
-
 describe('API /api/users', () => {
-    let authToken = null; // Token JWT da aggiungere
-  
-    before(() => {
-      // Esegui una chiamata per ottenere un token JWT valido
-      cy.request({
-        method: 'POST',
-        url: 'http://localhost:8090/api/auth/login', // Assumi che ci sia un endpoint per il login che restituisce un token JWT
-        body: {
-          username: 'user-test',
-          password: 'grupposad',
-        },
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        authToken = response.body.token; // Salva il token JWT per usarlo nelle richieste successive
-      });
-    });
-  
-    it('should return a list of users', () => {
-      cy.request({
-        method: 'GET',
-        url: 'http://localhost:10000/api/users',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${authToken}`, // Includi il token JWT nelle intestazioni Authorization
-        },
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.length.greaterThan(0);
-        expect(response.body[0]).to.have.property('username');
+  let authToken = null;
 
-              // Stampare la lista di utenti nel terminale di Cypress
-            Cypress.log({
-              name: 'Users List',
-              consoleProps() {
-                // Questo oggetto verrà visualizzato nel log di Cypress
-                return {
-                  'Number of users': response.body.length,
-                  'All users': response.body,
-                };
-              },
-            });
-             // Utilizzo di console.log per stampare le informazioni nel terminale
-                console.log('Number of users:', response.body.length);
-                console.log('First user:', response.body[0]);
-                console.log('All users:', response.body);
-      });
+  before(() => {
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:8090/api/auth/login',
+      body: {
+        username: 'user-test',
+        password: 'grupposad',
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      authToken = response.body.token;
+      cy.log('Auth Token Acquired');
+      console.log('Auth Token Acquired:', authToken);
     });
   });
-  
+
+  it('Dovrebbe ritornare una lista di utenti', () => {
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:10000/api/users',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.length.greaterThan(0);
+      expect(response.body[0]).to.have.property('username');
+//per visualizzare la lista di utenti guardalo da npx cypress open, dalla gui del browser
+      cy.log('Number of users: ' + response.body.length);
+      cy.log('First user: ' + JSON.stringify(response.body[0]));
+      cy.log('All users: ' + JSON.stringify(response.body));
+
+      console.log('Number of users:', response.body.length);
+      console.log('First user:', response.body[0]);
+      console.log('All users:', response.body);
+    });
+  });
+});
